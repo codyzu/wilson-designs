4e1aac199abeee6791798b36cd01ce91307045ac
codyzu <cody.zuschlag@gmail.com>
2022-06-14 22:53:05 +0200

    add cover image

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col } from "reactstrap";
-import { Link, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import cx from "classnames";
 import classes from "./home.module.css";
 import { BsPhone } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
@@ -42,59 +43,78 @@ export default function Home() {
   }
 
   return (
-    <Container>
-      <Row className="justify-content-center my-5">
-        <Col xs="auto" className={classes.funky}>
-          <h1 className="fw-bold d-inline-flex flex-column align-items-center">
-            <span>Wilson</span>
-            <span>Designs</span>
-          </h1>
-        </Col>
-      </Row>
-      <Row className="justify-content-center mb-3">
-        <Col xs="auto">
-          <h4>Annecy, France</h4>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col xs="auto">
-          <a href="tel:+330612345678" className="text-decoration-none">
-            <BsPhone className="me-2" />
-            +33 6 12 34 56 78
-          </a>
-        </Col>
-        <Col xs="auto">
-          <a
-            href="mailto:kevin@wilson-designs.fr"
-            className="text-decoration-none"
-          >
-            <AiOutlineMail className="me-2" />
-            kevin@wilson-designs.fr
-          </a>
-        </Col>
-      </Row>
-      <Row className="g5">
-        {images.map(([path, module]) => (
-          <Col key={path} xs={12} sm={6} lg={4} className="p-5">
-            <a
-              href={`/?${new URLSearchParams({ photo: path }).toString()}`}
-              onClick={(e) => {
-                e.preventDefault();
-                setSearchParams({ photo: path });
-              }}
-            >
-              <div className="ratio ratio-16x9 justify-content-center">
-                <img
-                  src={module.default}
-                  className="img-fluid rounded shadow-lg mw-100 mh-100"
-                  style={{ objectFit: "cover" }}
-                  loading="lazy"
-                />
-              </div>
+    <>
+      <div
+        className={cx(
+          "vw-100 vh-100 overflow-hidden text-center d-flex justify-content-center mw-100",
+          classes.funky
+        )}
+        style={{
+          backgroundImage:
+            'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("/alps.jpg")',
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <h1 className="fw-bold d-inline-flex flex-column align-items-center align-self-center">
+          <span>Wilson</span>
+          <span>Designs</span>
+        </h1>
+      </div>
+      <Container className="mt-5">
+        {/* <Row className="justify-content-center my-5">
+          <Col xs="auto" className={classes.funky}>
+            <h1 className="fw-bold d-inline-flex flex-column align-items-center">
+              <span>Wilson</span>
+              <span>Designs</span>
+            </h1>
+          </Col>
+        </Row> */}
+        <Row className="justify-content-center mb-3">
+          <Col xs="auto">
+            <h4>Annecy, France</h4>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs="auto">
+            <a href="tel:+330612345678" className="text-decoration-none">
+              <BsPhone className="me-2" />
+              +33 6 12 34 56 78
             </a>
           </Col>
-        ))}
-      </Row>
-    </Container>
+          <Col xs="auto">
+            <a
+              href="mailto:kevin@wilson-designs.fr"
+              className="text-decoration-none"
+            >
+              <AiOutlineMail className="me-2" />
+              kevin@wilson-designs.fr
+            </a>
+          </Col>
+        </Row>
+        <Row className="g5">
+          {images.map(([path, module]) => (
+            <Col key={path} xs={12} sm={6} lg={4} className="p-5">
+              <a
+                href={`/?${new URLSearchParams({ photo: path }).toString()}`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSearchParams({ photo: path });
+                }}
+              >
+                <div className="ratio ratio-16x9 justify-content-center">
+                  <img
+                    src={module.default}
+                    className="img-fluid rounded shadow-lg mw-100 mh-100"
+                    style={{ objectFit: "cover" }}
+                    loading="lazy"
+                  />
+                </div>
+              </a>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
   );
 }
