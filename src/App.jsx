21821babdbf28eaadb422e38a635a0dd4ac3c094
codyzu <cody.zuschlag@gmail.com>
2022-06-14 22:31:50 +0200

    debug deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./Home";
 
 function App() {
+  console.log("base:", import.meta.env.BASE_URL);
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
